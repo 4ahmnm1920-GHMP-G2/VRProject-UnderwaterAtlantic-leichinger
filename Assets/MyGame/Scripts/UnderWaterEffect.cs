9769033ea3ccdbb9d65ca533d6e0556a5113a00d
leoeichinger <eichinger.l@gmx.at>
2020-01-27 14:45:51 +0100

    redid the underwater shader image effect

--- Assets/MyGame/Scripts/UnderWaterEffect.cs
+++ Assets/MyGame/Scripts/UnderWaterEffect.cs
@@ -2,18 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+[ExecuteInEditMode ]
 public class UnderWaterEffect : MonoBehaviour
 {
     public Material _mat;
-    [Range(0.001f, 0.1f)]
-    public float _pixelOffset;
-    [Range(0.1f, 20f)]
-    public float _noiseScale;
-    [Range(0.1f, 20f)]
-    public float _noiseFrequency;
-    [Range(0.1f, 30f)]
-    public float _noiseSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +15,7 @@ public class UnderWaterEffect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _mat.SetFloat("NoiseFrequency", _noiseFrequency);
-        _mat.SetFloat("NoiseSpeed", _noiseSpeed);
-        _mat.SetFloat("NoiseScale", _noiseScale);
-        _mat.SetFloat("_PixelOffset", _pixelOffset);
+        
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest) 
