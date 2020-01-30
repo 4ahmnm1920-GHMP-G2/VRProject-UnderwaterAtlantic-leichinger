25b1b1ff1b6192e53dfd7d916ede3743ce222fb2
leoeichinger <eichinger.l@gmx.at>
2020-01-30 10:37:05 +0100

    yeeet

---
+++ Assets/Scripts/AverageColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AverageColor : MonoBehaviour {
+
+	public Color averageCol(Texture2D tex)
+    {
+        var texColors = tex.GetPixels32();
+        var total = texColors.Length;
+        var r = 0;
+        var g = 0;
+        var b = 0;
+        for (int i = 0; i < total; i++)
+        {
+            r += texColors[i].r;
+            g += texColors[i].g;
+            b += texColors[i].b;
+        }
+
+        return new Color(r/total, g/total, b/total, 0);
+    }
+}
